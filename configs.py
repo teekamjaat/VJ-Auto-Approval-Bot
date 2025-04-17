a06e17a493d2b848a742ef5bcd63bef3201bc20d
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-04-17 20:19:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22349465"))
     API_HASH = getenv("API_HASH", "3732e079c4125690226d8e7b4e028ca4")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7497272339:AAEVO9dW3yBlqluX1ecU6GOmXimjVFWoqUk")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7497272339:AAGy8TNhAEVsQ1V6P6wcAU3iBXgscCZXz7E")
     # Your Force Subscribe Channel Id Below 
     CHID = int(getenv("CHID", "-1002273359017")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
